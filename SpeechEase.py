6a380df0815f8aedd9d71d1fc5479b4152d7ad4f
Parangot09 <105841513+Parangot09@users.noreply.github.com>
2023-04-15 21:07:59 +0530

    Documented the code

--- SpeechEase.py
+++ SpeechEase.py
@@ -1,3 +1,4 @@
+# import required modules
 import tkinter.filedialog
 from tkinter import *
 import customtkinter
@@ -7,7 +8,7 @@ from  tkinter import  ttk
 from tkinter.filedialog import askdirectory
 import tkinter as tk
 
-
+# initialize text-to-speech engine
 txt = pyttsx3.init()
 win = Tk()
 win.geometry("800x600")
@@ -18,50 +19,43 @@ sidebar.place(x=0,y=0)
 win.resizable(False,False)
 win.iconbitmap('C:\\Users\\hp\\Documents\\tkinter\\Txtv2\\Txt.ico')
 
+# create a label widget
 label = customtkinter.CTkLabel(master=win,text="Voice setting",text_font="Courier",bg="#202442",text_color="white")
-label.place(x=10,y=200)
+label.place(x=10,y=200
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#clear function
+# define a function to clear the textbox
 def cle_r():
     box.textbox.delete("0.1",END)
+
+# define a function to save the audio file
 def saver():
     inp = box.textbox.get("0.1",END)
     path =  tkinter.filedialog.asksaveasfilename(title="Save as",filetypes=(("MP3 files", "*.mp3"), ("WAV files", "*.wav"), ("All files", "*.*"))
 )
     txt.save_to_file(inp,path)
     txt.runAndWait()
+
+# define a function to read the text aloud
 def say():
     inp = box.textbox.get("0.1", END)
     txt.say(inp)
     txt.runAndWait()
 
-
+# define a function to select the female voice
 def female():
     voices = txt.getProperty('voices')
     txt.setProperty('voice', voices[1].id)
 
     txt.runAndWait()
 
+# define a function to select the male voice
 def male():
     voices = txt.getProperty('voices')
     txt.setProperty('voice', voices[0].id)
 
     txt.runAndWait()
 
-
+# define a function to open a file
 def open_file():
     file_path = tk.filedialog.askopenfilename(defaultextension=".docx", filetypes=[("Word Document", "*.docx")])
     if file_path:
@@ -70,15 +64,13 @@ def open_file():
             box.textbox.delete("1.0", "end")
             box.textbox.insert("end", text)
 
-
-
-
+# create a textbox widget
 box = customtkinter.CTkTextbox(master=win,fg_color="#2D325A",width=520,height=242,border_color="#7033FF",border_width=1,corner_radius=10,text_color="white",
   text_font="Courier"                                 )
 box.place(x=202,y=113)
 
 
-#defining button
+# create button widgets
 
 b1 = customtkinter.CTkButton(master=win,text_color="white",text_font="Courier",fg_color="#2D325A",bg_color="#252A4B",text="Clear",corner_radius=10,
          border_color="#43B5B5",border_width=1,command=cle_r)
